f47403d25357f7cb06a3af6d7a95f6af6df19c04
austruyfabrice@yahoo.fr <austruyfabrice@yahoo.fr>
2017-11-26 17:09:29 +0100

    initial commit

--- app.js
+++ app.js
@@ -5,47 +5,99 @@ var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 5000));
-// app.listen((5000));
+app.listen((process.env.PORT || 5000));   // allow dynamic port allocation; required for Heroku hosting
 
 // Server index page
 app.get("/", function (req, res) {
-  res.send("Deployed!");
+  //res.send("deployed!");
+  
+  response = "Coucou Guillemot!" //Default response from the webhook to show it's working
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response 
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+  
+  //res.sendFile(__dirname+'/simple.html');
+  
 });
 
 // Elaborating parser
 app.get("/parser", function (req, res) {
   
-	console.log(req.query.q);
-	
-	//////////////////	
+  	//////////////////	
 	//request parsing
 	/////////////////
+	
+	try{ 
+	
 	var tokens = req.query.q.split(' ');
-		
-	if (tokens.length == 0) {res.send("error: no argument in query");}
+	
+	console.log(" ");
+	console.log("  -- starting parser -- ");
+	console.log("parser was invoked with following parameters: " + req.query.q.toString());
+
+	//////////////////	
+	//token conversion to lower case strings
+	/////////////////
+	
+	var tokens_lower_case = [];
+	
+	for (i = 0; i < tokens.length; ++i) {
+		tokens_lower_case.push(tokens[i].toString().toLowerCase());
+	}
+	console.log("tokens lower case: "+tokens_lower_case);
 	
 	//////////////////	
 	//token detection
 	/////////////////
 	
-	for (index = 0; index < tokens.length; ++index) {
+	
+	var flag_m ;   // set to true if the key word "m"  or "M" is in the query
+	var flag_m1 ;  // set to true if the key word "m1" or "M1" is in the query
+	var flag_m2 ;  // set to true if the key word "m2" or "M2" is in the query
+	var flag_a0 ;  // set to true if the key word "a0" or "A0" is in the query
+	var flag_a1 ;  // set to true if the key word "a1" or "A1" is in the query
+	var flag_a2 ;  // set to true if the key word "a2" or "A2" is in the query
+	var flag_a3 ;  // set to true if the key word "a3" or "A3" is in the query
+	var flag_b1 ;  // set to true if the key word "b1" or "B1" is in the query
+	var flag_b2 ;  // set to true if the key word "b2" or "B2" is in the query
+	
+	var non_key_words = [] ;  	// all the words of the query which are not keywords
+	var skills = []; 			// all the key words concerning skills (A0, A1...)
+		
+	for (index = 0; index < tokens_lower_case.length; ++index) {
     
-	if (tokens[index].toString() == "m")  { var flag_m = true  ;}
-	if (tokens[index].toString() == "m1") { var flag_m1 = true ;}
-	if (tokens[index].toString() == "m2") { var flag_m2 = true ;}
-	if (tokens[index].toString() == "a0") { var flag_a0 = true ;}
-	if (tokens[index].toString() == "a1") { var flag_a1 = true ;}
-	if (tokens[index].toString() == "a2") { var flag_a2 = true ;}
-	if (tokens[index].toString() == "a3") { var flag_a3 = true ;}
-	if (tokens[index].toString() == "b1") { var flag_b1 = true ;}
-	if (tokens[index].toString() == "b2") { var flag_b2 = true ;}
-	
-	console.log(tokens[index].toString());
-	
+		
+		if ((tokens_lower_case[index].toString() == "m"))   { var flag_m = true  ;}
+		else {
+		if ((tokens_lower_case[index].toString() == "m1")) { var flag_m1 = true ;}
+		else {
+		if ((tokens_lower_case[index].toString() == "m2")) { var flag_m2 = true ;}
+		else {
+		if ((tokens_lower_case[index].toString() == "a0")) { var flag_a0 = true ; skills.push("a0");}
+		else {
+		if ((tokens_lower_case[index].toString() == "a1")) { var flag_a1 = true ; skills.push("a1");}
+		else {
+		if ((tokens_lower_case[index].toString() == "a2")) { var flag_a2 = true ; skills.push("a2");}
+		else {
+		if ((tokens_lower_case[index].toString() == "a3")) { var flag_a3 = true ; skills.push("a3");}
+		else {
+		if ((tokens_lower_case[index].toString() == "b1")) { var flag_b1 = true ; skills.push("b1");}
+		else {
+		if ((tokens_lower_case[index].toString() == "b2")) { var flag_b2 = true ; skills.push("b2");}
+		else {
+			non_key_words.push(tokens_lower_case[index].toString());
+		}}}}}}}}}
+		
+		console.log("word detected in request:" + tokens_lower_case[index].toString());
+			
 	}
 	
-	//res.send(req.query);
+	//////////////////
+	// just for debug...
+	//////////////////
+	console.log("non key words detected in request:" + non_key_words);
+	console.log("skills detected in request:" + skills);
 	
 	
 	//////////////////	
@@ -56,49 +108,74 @@ app.get("/parser", function (req, res) {
 	csv.parseCSV("data.csv", function(data){
 	//console.log(data);
 		
+		
 		//////////////////	
-		//parsing data
+		//parsing raw data
 		/////////////////
 
 		var query = require('array-query');	
 		var result;
 		
 		//////////////////
-		//par defaut, tri sur la colonne M
+		//sorting data : by defaut, descending sort on currenth month
 		//////////////////
-		result = query().sort("M").desc().on(data);
-					
-		// if (flag_m) 	{ result = query().sort("M").desc().on(data);}
-		if (flag_m1)	{ result = query().sort("M1").desc().on(data);}
-		if (flag_m2)	{ result = query().sort("M2").desc().on(data);}
+		result = query().sort("m").desc().on(data);
+		if (flag_m1)	{ result = query().sort("m1").desc().on(data);}
+		if (flag_m2)	{ result = query().sort("m2").desc().on(data);}
 		
-		var skills = [];
 		
-		if (flag_a0){skills.push("A0");}
-		if (flag_a1){skills.push("A1");}
-		if (flag_a2){skills.push("A2");}
-		if (flag_a3){skills.push("A3");}
-		if (flag_b1){skills.push("B1");}
-		if (flag_b2){skills.push("B2");}
-		
-		console.log(skills);
-		
+		//////////////////
+		//filtering data on skills
+		//////////////////
 		if (skills.length != 0) {
-			result = query("Titre").within(skills).on(result);
+			result = query("titre").within(skills).on(result);
 		}
 		
-		//var small_data = query("M1").gt(21).on(data);
+		//////////////////
+		//filtering data with non key fields as plain text query
+		//////////////////
+		if (non_key_words.length != 0) {
+
+			buffer = result ;
+			
+			/////////////////
+			// we search for the keyword in all the "Tri" column
+			/////////////////
+			
+			/////////////////
+			// convert the non key words in a search string 
+			/////////////////
+			
+			var non_key_words_as_string = non_key_words[0].toString(); //
+			for (j = 1; j < non_key_words.length; ++j) {
+				non_key_words_as_string = non_key_words_as_string + " " + non_key_words[j];
+			}
+			
+			console.log("non key words as string: " + non_key_words_as_string);
+			result = query("rd").search(non_key_words_as_string).or("nom").search(non_key_words_as_string).or("trg").search(non_key_words_as_string).on(result);
+					
+		}
 		
-		//////////////////	
-		//sending data
+	
+		/////////////////	
+		//sending output 
 		/////////////////
-		
+	
 		res.send(result);
 
 	});	
 		
 	
 	
+	
+	
+		
+	}
+	
+	catch (err) {	
+	console.log("error: can not proces request");
+	res.send("error: can not proces request");
+	}
 	
 });
 
