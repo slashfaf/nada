5daaa29ebdeaf627fb7fb3deba7b3935dc3be208
austruyfabrice@yahoo.fr <austruyfabrice@yahoo.fr>
2018-01-07 12:11:02 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
-
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));   // allow dynamic port allocation; required for Heroku hosting
+
 
 // Server index page
 app.get("/", function (req, res) {
