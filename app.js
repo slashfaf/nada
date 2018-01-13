48cd1076c16c0dda55d75e12dbad437ef63516b7
austruyfabrice@yahoo.fr <austruyfabrice@yahoo.fr>
2018-01-13 11:31:52 +0100

    on y est presque...

--- app.js
+++ app.js
@@ -364,6 +364,7 @@ function lister_les_consultants_disponibles(callback, nom, grade, date) {
 			
 			case "m2" :
 			m2_formating = "*";
+			break 
 			
 			default :
 			m_formating = "*"
@@ -371,8 +372,8 @@ function lister_les_consultants_disponibles(callback, nom, grade, date) {
 		
 		result.forEach(function(item){
 			
-			console.log(item.m + ", " + item.m1 + ", " + item.m2 + ", " + item.nom + ", " + item.titre + ", "  );
-			result_as_string =  result_as_string + m_formating + four_digits_string(item.m) + m_formating + "," + m1_formating + four_digits_string(item.m1) + m1_formating + "," + m2_formating + four_digits_string(item.m2) + m2_formating + "," +  item.nom + "," + item.titre +  " \n " ;
+			console.log(item.m + " " + item.m1 + " " + item.m2 + " " + item.nom + " " + item.titre + " "  );
+			result_as_string =  result_as_string + m_formating + four_digits_string(item.m) + m_formating + " " + m1_formating + four_digits_string(item.m1) + m1_formating + " " + m2_formating + four_digits_string(item.m2) + m2_formating + " " + "*" + item.nom + "*" + " " + item.titre +  " \n " ;
 			
 		});
 		
